cc23ff76b6f019fa5f8550f5b820c2f091e80b2d
Nikolas <nkls99@gmail.com>
2023-03-12 21:39:27 -0300

    required amount to be positive

--- src/middlewares/schemas/cartSchemas.ts
+++ src/middlewares/schemas/cartSchemas.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 
 export const AddOrUpdateSchema = Joi.object<ProductOnCart>({
   productId: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required(),
-  amount: Joi.number().integer().required()
+  amount: Joi.number().integer().positive().required()
 })
 
 export const DeleteSchema = Joi.object({
